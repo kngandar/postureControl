cae0ea8d4fb9f3dab35d97c40708fc6fbf12a249
GJGilles <kngandar@gmail.com>
2019-03-04 14:38:50 -0500

    Changed arduino to get sensor value upon request, Updated postureCases according to testing, Updated test_main to laptop code, Main code is a modified version of test_main with cleaner code and better data logging, Removedamanda arduino file

--- src/SeatSensor_DataCollection.ino
+++ src/SeatSensor_DataCollection.ino
@@ -4,6 +4,7 @@
 
 #include "HX711.h"
 
+//Chair Seat
 #define DOUT 2
 #define CLK 3
 #define DOUT2 4
@@ -13,23 +14,42 @@
 #define DOUT4 8
 #define CLK4 9
 
+//Chair Back
+#define DOUT5 10
+#define CLK5 11
+#define DOUT6 12
+#define CLK6 13
+#define DOUT7 16
+#define CLK7 17
+#define DOUT8 18
+#define CLK8 19
+
 // Initialize load cell sensor objects
-HX711 frontR(DOUT, CLK);
-HX711 frontL(DOUT2, CLK2);
-HX711 backR(DOUT3, CLK3);
+HX711 backR(DOUT, CLK);
+HX711 frontR(DOUT2, CLK2);
+HX711 frontL(DOUT3, CLK3);
 HX711 backL(DOUT4, CLK4);
 
+HX711 lowerR(DOUT5, CLK5);
+HX711 lowerL(DOUT6, CLK6);
+HX711 upperL(DOUT7, CLK7);
+HX711 upperR(DOUT8, CLK8);
+
 // Calibration factors
-float cf_frontR = 280000;
-float cf_frontL = 774000;
-float cf_backR = 840000;
-float cf_backL = 881000;
+float cf_backR = 550000;
+float cf_frontR = 650000;
+float cf_frontL = 480000;
+float cf_backL = 650000;
+
+float cf_lowerR = -22500;
+float cf_lowerL = -21000;
+float cf_upperL = -7500;
+float cf_upperR = -7500;
+
+char msg;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("HX711 data collection sketch");
-  Serial.println("Remove all weight from scale");
-  Serial.println("After readings begin, place known weight on scale");
 
   // Resets scale to 0
   frontR.set_scale();
@@ -41,51 +61,66 @@ void setup() {
   backL.set_scale();
   backL.tare();
 
+  upperR.set_scale();
+  upperR.tare();
+  upperL.set_scale();
+  upperL.tare();
+  lowerR.set_scale();
+  lowerR.tare();
+  lowerL.set_scale();
+  lowerL.tare();
+  
   // Set calibration factor
   frontR.set_scale(cf_frontR);
   frontL.set_scale(cf_frontL);
   backR.set_scale(cf_backR);
   backL.set_scale(cf_backL);
+  
+  upperR.set_scale(cf_upperR);
+  upperL.set_scale(cf_upperL);
+  lowerR.set_scale(cf_lowerR);
+  lowerL.set_scale(cf_lowerL);
 }
 
 void loop() {
   // Print sensor values
-  Serial.print("Reading front right: ");
-  Serial.print(frontR.get_units(), 1);
-  Serial.print(" kg"); 
-  Serial.print(" calibration_factor: ");
-  Serial.print(cf_frontR);
-  Serial.println();
 
-  Serial.print("Reading front left: ");
-  Serial.print(frontL.get_units(), 1);
-  Serial.print(" kg"); 
-  Serial.print(" calibration_factor: ");
-  Serial.print(cf_frontL);
-  Serial.println();
-
-  Serial.print("Reading back right: ");
-  Serial.print(backR.get_units(), 1);
-  Serial.print(" kg"); 
-  Serial.print(" calibration_factor: ");
-  Serial.print(cf_backR);
-  Serial.println();
-
-  Serial.print("Reading back left: ");
-  Serial.print(backL.get_units(), 1);
-  Serial.print(" kg"); 
-  Serial.print(" calibration_factor: ");
-  Serial.print(cf_backL);
-  Serial.println();
-
-  // Displays data after every 250ms
-  delay(250);
-
-  // Quit the program if enter 'q' or 'Q'
   if(Serial.available())
   {
-    char temp = Serial.read();
-    if (temp == 'q' || temp == 'Q')
-      return;
+
+    msg = Serial.read();
+      
+	//front right
+	Serial.print(frontR.get_units(),1);
+	Serial.print(", ");
+
+	//front left
+	Serial.print(frontL.get_units(),1);
+	Serial.print(", ");
+
+	//back right
+	Serial.print(backR.get_units(), 1);
+	Serial.print(", ");
+
+	//back left
+	Serial.print(backL.get_units(), 1);
+	Serial.print(", ");
+
+	//upper left
+	Serial.print(upperL.get_units(), 1);
+	Serial.print(", ");
+
+	//upper right
+	Serial.print(upperR.get_units(), 1);
+	Serial.print(", ");
+
+	//lower left
+	Serial.print(lowerL.get_units(), 1);
+	Serial.print(", ");
+
+	//lower right
+	Serial.println(lowerR.get_units(), 1);
+
   }
-}
+
+ 
--- src/postureCases.py
+++ src/postureCases.py
@@ -13,82 +13,17 @@ GPIO.setup(35,GPIO.OUT)     # Lumbar
 
 vibrateTime = 2
 
-def correctUpright(FR, FL, BR, BL):
-    if FR == 0 and FL == 0 and BR == 0 and BL == 0:
+def correctUpright(FR, FL, BR, BL, UL, UR, LL, LR):
+    if FR == 0 and FL == 0 and BR == 0 and BL == 0 and \
+            (UL == 0 or UR == 0) and LL == 0 and LR == 0:
         # No motor action done
         return True
     else:
         return False
 
-
-def leanForward(FR, FL, BR, BL):
-    if FR > 0 and FL > 0 and BR < 0 and BL < 0:
-        # Lumbar on
-        GPIO.output(35,GPIO.HIGH)
-        time.sleep(vibrateTime)
-        GPIO.output(35,GPIO.LOW)
-        return True
-    else:
-        return False
-
-
-def leanBackward(FR, FL, BR, BL):
-    if FR < 0 and FL < 0 and BR > 0 and BL > 0:
-        # Back on
-        GPIO.output(33,GPIO.HIGH)
-        time.sleep(vibrateTime)
-        GPIO.output(33,GPIO.LOW)
-        return True
-    else:
-        return False
-
-
-def leanLeft(FR, FL, BR, BL):
-    if FR < 0 and FL > 0 and BR < 0 and BL > 0:
-        # Left on
-        GPIO.output(29, GPIO.HIGH)
-        time.sleep(vibrateTime)
-        GPIO.output(29, GPIO.LOW)
-        return True
-    else:
-        return False
-
-
-def leanRight(FR, FL, BR, BL):
-    if FR > 0 and FL < 0 and BR > 0 and BL < 0:
-        # Right on
-        GPIO.output(31, GPIO.HIGH)
-        time.sleep(vibrateTime)
-        GPIO.output(31, GPIO.LOW)
-        return True
-    else:
-        return False
-
-
-def twistLeft(FR, FL, BR, BL):
-    if FR <= 0 and FL >= 0 and BR <= 0 and BL >= 0:
-        # Left on
-        GPIO.output(29, GPIO.HIGH)
-        time.sleep(vibrateTime)
-        GPIO.output(29, GPIO.LOW)
-        return True
-    else:
-        return False
-
-
-def twistRight(FR, FL, BR, BL):
-    if FR >= 0 and FL <= 0 and BR >= 0 and BL <= 0:
-        # Right on
-        GPIO.output(31, GPIO.HIGH)
-        time.sleep(vibrateTime)
-        GPIO.output(31, GPIO.LOW)
-        return True
-    else:
-        return False
-
-
-def slouching(FR, FL, BR, BL):
-    if FR >= 0 and FL >= 0 and BR <= 0 and BL <= 0:
+def leanForward(FR, FL, BR, BL, UL, UR, LL, LR):
+    if FR >= 0 and FL >= 0 and BR <= 0 and BL <= 0 and \
+            (UL < 0 or UR < 0) and LL >= 0 and LR >= 0:
         # Lumbar on
         GPIO.output(35, GPIO.HIGH)
         time.sleep(vibrateTime)
@@ -96,3 +31,48 @@ def slouching(FR, FL, BR, BL):
         return True
     else:
         return False
+
+def leanBackward(FR, FL, BR, BL, UL, UR, LL, LR):
+    if FR < 0 and FL < 0 and BR >= 0 and BL >= 0 and \
+            LL >= 0 and LR >= 0:
+        # Back on
+        GPIO.output(33, GPIO.HIGH)
+        time.sleep(vibrateTime)
+        GPIO.output(33, GPIO.LOW)
+        return True
+    else:
+        return False
+
+def leanLeft(FR, FL, BR, BL, UL, UR, LL, LR):
+    if FR < 0 and FL >= 0 and BR < 0 and BL >= 0:
+        # Left on
+        GPIO.output(29, GPIO.HIGH)
+        time.sleep(vibrateTime)
+        GPIO.output(29, GPIO.LOW)
+        return True
+    else:
+        return False
+
+def leanRight(FR, FL, BR, BL, UL, UR, LL, LR):
+    if FR >= 0 and FL < 0 and BR >= 0 and BL < 0:
+        # Right on
+        GPIO.output(31, GPIO.HIGH)
+        time.sleep(vibrateTime)
+        GPIO.output(31, GPIO.LOW)
+        return True
+    else:
+        return False
+
+def sitForwards(FR, FL, BR, BL, UL, UR, LL, LR):
+    if FR >= 0 and FL >= 0 and BR <= 0 and BL <= 0 and \
+            (UL < 0 or UR < 0) and LL < 0 and LR < 0:
+        # Left + Right on
+        GPIO.output(29, GPIO.HIGH)
+        GPIO.output(31, GPIO.HIGH)
+        time.sleep(vibrateTime)
+        GPIO.output(29, GPIO.LOW)
+        GPIO.output(31, GPIO.LOW)
+        return True
+    else:
+        return False
+
